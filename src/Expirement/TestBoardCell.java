ab29eadde36ade07d5a8fed926e1628991e9e0f7
HenryJohnson26 <108373639+HenryJohnson26@users.noreply.github.com>
2022-10-05 15:19:36 -0600

    added instance data constructor setters and getters for TestBoardCell

--- src/Expirement/TestBoardCell.java
+++ src/Expirement/TestBoardCell.java
@@ -1,9 +1,40 @@
 package Expirement;
 
-public class TestBoardCell {
+import java.util.HashSet;
+import java.util.Set;
 
+public class TestBoardCell {
+	private int row;
+	private int column;
+	private Set<TestBoardCell> adjacencyList;
+	private boolean isRoom;
+	private boolean isOccupied;
 	
 	public TestBoardCell(int r, int c) {
-		
+		adjacencyList = new HashSet<>();
+		row = r;
+		column = c;
 	}
+	
+	public void addAdjacency( TestBoardCell cell) {
+		adjacencyList.add(cell);
+	}
+	
+	public Set<TestBoardCell> getAdjList(){
+		return adjacencyList;
+	}
+	
+	public void setRoom(boolean isRoom) {
+		this.isRoom = isRoom;
+	}
+	public boolean isRoom() {
+		return isRoom;
+	}
+	public void setOccupied(boolean occupied) {
+		isOccupied = occupied;
+	}
+	public boolean isOccupied() {
+		return isOccupied;
+	}
+	
 }

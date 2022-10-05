d544f35845d7e219288708088219b7368b6577f3
HenryJohnson26 <108373639+HenryJohnson26@users.noreply.github.com>
2022-10-05 15:25:54 -0600

    added method stubs

--- src/Expirement/TestBoard.java
+++ src/Expirement/TestBoard.java
@@ -1,5 +1,22 @@
 package Expirement;
 
-public class TestBoard {
+import java.util.HashSet;
+import java.util.Set;
 
+public class TestBoard {
+	private Set<TestBoardCell> targets;
+	
+	public TestBoard() {
+		targets = new HashSet<>();
+	}
+	public void calcTargets( TestBoardCell startCell, int pathlength) {
+		
+	}
+	public Set<TestBoardCell> getTargets(){
+		return targets;
+	}
+	public TestBoardCell getCell( int row, int col) {
+		return null;
+	}
+	
 }

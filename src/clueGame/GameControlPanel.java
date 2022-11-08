b1708a6d75a533d717d6af703eefe31558d1e1cb
Isaiah Stene <istene@mines.edu>
2022-11-08 13:07:40 -0700

    Created GameControlPanel class.

---
+++ src/clueGame/GameControlPanel.java
@@ -0,0 +1,27 @@
+package clueGame;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GameControlPanel extends JPanel {
+	
+	public GameControlPanel() {
+		
+	}
+	
+	//main to test panel
+	public static void main(String[] args) {
+		GameControlPanel panel = new GameControlPanel();  // create the panel
+		JFrame frame = new JFrame();  // create the frame 
+		frame.setContentPane(panel); // put the panel in the frame
+		frame.setSize(750, 180);  // size the frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
+		frame.setVisible(true); // make it visible
+		
+		// test filling in the data
+//		panel.setTurn(new ComputerPlayer( "Col. Mustard", 0, 0, "orange"), 5);
+//		panel.setGuess( "I have no guess!");
+//		panel.setGuessResult( "So you have nothing?");
+	}
+
+}

5789aad35a26aa384e6e605a91e880347851deee
HenryJohnson26 <108373639+HenryJohnson26@users.noreply.github.com>
2022-11-11 15:42:48 -0700

    updating cards works but resizing needs to be done

--- src/clueGame/KnownCardsPanel.java
+++ src/clueGame/KnownCardsPanel.java
@@ -1,6 +1,7 @@
 package clueGame;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,6 +11,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 public class KnownCardsPanel extends JPanel{
 
@@ -29,24 +32,38 @@ public class KnownCardsPanel extends JPanel{
 	private int roomPanelSize;
 	private int playerPanelSize;
 	private int weaponPanelSize;
-	private Board testBoard = Board.getInstance();
+	private int roomHandSize;
+	private int playerHandSize;
+	private int weaponHandSize;
+	private static Board testBoard = Board.getInstance();
 
 
 	public KnownCardsPanel() {
-		roomPanelSize = 0;
-		playerPanelSize = 0;
-		weaponPanelSize = 0;
-		mainPanel = new JPanel(new GridLayout(0,3));
-		roomPanel = new JPanel(new GridLayout(0,2));
-		playerPanel = new JPanel(new GridLayout(0,2));
-		weaponPanel = new JPanel(new GridLayout(0,2));
-		handRoomPanel = new JPanel(new GridLayout(0,3));
-		handPlayerPanel = new JPanel(new GridLayout(0,3));
-		handWeaponPanel = new JPanel(new GridLayout(0,3));
+		roomPanelSize = 1;
+		playerPanelSize = 1;
+		weaponPanelSize = 1;
+		roomHandSize = 1;
+		playerHandSize = 1;
+		weaponHandSize = 1;
+		mainPanel = new JPanel(new GridLayout(3, 1));
+		mainPanel.setBorder(new TitledBorder(new EtchedBorder(), "Known Cards:"));
+		roomPanel = new JPanel(new GridLayout(2, 1));
+		roomPanel.setPreferredSize(new Dimension(100, 100));
+		roomPanel.setBorder(new TitledBorder(new EtchedBorder(), "Rooms:"));
+		playerPanel = new JPanel(new GridLayout(2,1));
+		playerPanel.setBorder(new TitledBorder(new EtchedBorder(), "People:"));
+		weaponPanel = new JPanel(new GridLayout(2,1));
+		weaponPanel.setBorder(new TitledBorder(new EtchedBorder(), "Weapons:"));
+		handRoomPanel = new JPanel(new GridLayout(1,1));
+		handPlayerPanel = new JPanel(new GridLayout(1, 1));
+		handWeaponPanel = new JPanel(new GridLayout(1, 1));
+		handRoomPanel.add(new JLabel("In Hand:"));
+		handPlayerPanel.add(new JLabel("In Hand:"));
+		handWeaponPanel.add(new JLabel("In Hand:"));
 		createHandPanels();
-		seenRoomPanel = new JPanel(new GridLayout(roomPanelSize, 0));
-		seenPlayerPanel = new JPanel(new GridLayout(playerPanelSize, 0));
-		seenWeaponPanel = new JPanel(new GridLayout(weaponPanelSize, 0));
+		seenRoomPanel = new JPanel(new GridLayout(roomPanelSize, 1));
+		seenPlayerPanel = new JPanel(new GridLayout(playerPanelSize, 1));
+		seenWeaponPanel = new JPanel(new GridLayout(weaponPanelSize, 1));
 		roomPanel.add(handRoomPanel);
 		playerPanel.add(handPlayerPanel);
 		weaponPanel.add(handWeaponPanel);
@@ -62,48 +79,87 @@ public class KnownCardsPanel extends JPanel{
 		mainPanel.add(roomPanel);
 		mainPanel.add(playerPanel);
 		mainPanel.add(weaponPanel);
+		mainPanel.setSize(250, 100);
 	}
 	private void createHandPanels() {
+		int roomCounter = 0;
+		int playerCounter = 0;
+		int weaponCounter = 0;
 		for(Card card : testBoard.getHumanPlayer().getHand()) {
-			int roomCounter = 0;
-			int playerCounter = 0;
-			int weaponCounter = 0;
 			switch(card.getType()){
 			case PLAYER:
-				JLabel label = new JLabel(card.getName());
+				JTextField label = new JTextField(card.getName());
 				label.setBackground(testBoard.getHumanPlayer().getPlayerColor());
-				handRoomPanel.add(label);
-				roomCounter++;
+				label.setEditable(false);
+				playerHandSize ++;
+				handPlayerPanel.setLayout(new GridLayout(playerHandSize, 1));
+				handPlayerPanel.add(label);
+				playerCounter++;
 				break;
 			case WEAPON:
-				JLabel label1 = new JLabel(card.getName());
+				JTextField label1 = new JTextField(card.getName());
 				label1.setBackground(testBoard.getHumanPlayer().getPlayerColor());
-				handRoomPanel.add(label1);
+				label1.setEditable(false);
+				weaponHandSize ++;
+				handWeaponPanel.setLayout(new GridLayout(weaponHandSize, 1));
+				handWeaponPanel.add(label1);
 				weaponCounter++;
 				break;
 			default:
-				JLabel label2 = new JLabel(card.getName());
+				JTextField label2 = new JTextField(card.getName());
 				label2.setBackground(testBoard.getHumanPlayer().getPlayerColor());
+				label2.setEditable(false);
+				roomHandSize ++;
+				handRoomPanel.setLayout(new GridLayout(roomHandSize, 1));
 				handRoomPanel.add(label2);	
-				playerCounter++;
+				roomCounter++;
 				break;	
 			}
-			if(roomCounter == 0) {
-				handRoomPanel.add(new JLabel("None"));
-			}
-			if(playerCounter == 0) {
-				handPlayerPanel.add(new JLabel("None"));
-			}
-			if(weaponCounter == 0) {
-				handWeaponPanel.add(new JLabel("None"));
-			}
+		}
+		if(roomCounter == 0) {
+			JTextField field = new JTextField("None");
+			field.setEditable(false);
+			field.setSize(getPreferredSize());
+			handRoomPanel.add(field);
+		}
+		if(playerCounter == 0) {
+			JTextField field = new JTextField("None");
+			field.setEditable(false);
+			handPlayerPanel.add(field);
+		}
+		if(weaponCounter == 0) {
+			JTextField field = new JTextField("None");
+			field.setEditable(false);
+			handWeaponPanel.add(field);
 		}
 	}
 	public static void main(String[] args) {
+		testBoard.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+		testBoard.initialize();
 		KnownCardsPanel panel = new KnownCardsPanel();  // create the panel
 		JFrame frame = new JFrame();// create the frame 
+		JPanel wholeFrame = new JPanel(new BorderLayout());
+		wholeFrame.setLayout(new GridLayout(1, 1));
 		//set the file names to config files 
-		
+		for(Player player : testBoard.getPlayers()) {
+			if(player!=testBoard.getHumanPlayer()) {
+				for(Card card : player.getHand()) {
+					panel.updatePanel(card, player);
+				}
+			}
+		}
+		wholeFrame.add(mainPanel, BorderLayout.CENTER);
+		panel.add(wholeFrame);
+		frame.setContentPane(panel);
+		frame.setSize(250, 1000);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
+		frame.setVisible(true);
+//		for(Card card : testBoard.getDeck()) {
+//			if(!testBoard.getHumanPlayer().getHand().contains(card)) {
+//				updatePanel(card)
+//			}
+//		}
+
 	}
 	
 	public void updatePanel(Card card, Player player) {
@@ -132,7 +188,8 @@ public class KnownCardsPanel extends JPanel{
 		roomPanelSize++;
 		seenRoomPanel.removeAll();
 		roomPanel.removeAll();
-		seenRoomPanel.setLayout(new GridLayout(roomPanelSize, 0));
+		seenRoomPanel.setLayout(new GridLayout(roomPanelSize, 1));
+		seenRoomPanel.add(new JLabel("Seen:"));
 		if(seenRoomCards.isEmpty()) {
 			seenRoomPanel.add(new JLabel("None"));
 		}
@@ -152,7 +209,8 @@ public class KnownCardsPanel extends JPanel{
 		playerPanelSize++;
 		seenPlayerPanel.removeAll();
 		playerPanel.removeAll();
-		seenPlayerPanel.setLayout(new GridLayout(playerPanelSize, 0));
+		seenPlayerPanel.setLayout(new GridLayout(playerPanelSize, 1));
+		seenPlayerPanel.add(new JLabel("Seen:"));
 		if(seenPlayerCards.isEmpty()) {
 			seenPlayerPanel.add(new JLabel("None"));
 		}
@@ -172,7 +230,8 @@ public class KnownCardsPanel extends JPanel{
 		weaponPanelSize++;
 		seenWeaponPanel.removeAll();
 		weaponPanel.removeAll();
-		seenWeaponPanel.setLayout(new GridLayout(weaponPanelSize, 0));
+		seenWeaponPanel.setLayout(new GridLayout(weaponPanelSize, 1));
+		seenWeaponPanel.add(new JLabel("Seen:"));
 		if(seenWeaponCards.isEmpty()) {
 			seenWeaponPanel.add(new JLabel("None"));
 		}
